2dc9f47f16d1d907605575c64eef950c0d4ef6d6
AzizBenIsmail <aziz.270700@gmail.com>
2025-02-08 12:54:14 +0100

    recherche

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -7,6 +7,13 @@ router.post('/addUserClient',userController.addUserClient);
 router.post('/addUserAdmin',userController.addUserAdmin); 
 router.get('/getAllUsers',userController.getAllUsers); 
 router.get('/getUserById/:id',userController.getUserById); 
+router.get('/searchUserByUsername',userController.searchUserByUsername); 
+router.get('/getAllUsersAge/:age',userController.getAllUsersAge); 
+router.get('/getAllUsersSortByAge',userController.getAllUsersSortByAge); 
+router.get('/getAllClient',userController.getAllClient); 
+router.get('/getAllAdmin',userController.getAllAdmin); 
+router.get('/getAllUsersAgeBetMaxAgeMinAge',userController.getAllUsersAgeBetMaxAgeMinAge); 
+router.put('/updateuserById/:id',userController.updateuserById); 
 
 router.post('/addUserClientWithImg',upload.single("image_user"),userController.addUserClientWithImg); 
 
