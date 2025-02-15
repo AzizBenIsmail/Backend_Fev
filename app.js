29e1ba3ffc6bd1801c608d75b356aaf0be740f2b
AzizBenIsmail <aziz.270700@gmail.com>
2025-02-15 13:04:30 +0100

    gemini

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var indexRouter = require("./routes/indexRouter");
 var usersRouter = require("./routes/usersRouter");
 var osRouter = require("./routes/osRouter");
 var carRouter = require("./routes/carRouter");
+var GeminiRouter = require("./routes/GeminiRouter");
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/os", osRouter);
 app.use("/cars", carRouter);
+app.use("/Gemini", GeminiRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
